4dfd04b1d8633b5694c94b43a13dae6a6618de18
Äkwav <ekwav@coflnet.com>
2023-12-23 13:59:06 +0100

    generate client lib

--- Client/generate.sh
+++ Client/generate.sh
@@ -2,13 +2,16 @@ VERSION=0.0.1
 
 docker run --rm -v "${PWD}:/local" --network host -u $(id -u ${USER}):$(id -g ${USER})  openapitools/openapi-generator-cli generate \
 -i http://localhost:5033/swagger/v1/swagger.json \
--g csharp-netcore \
+-g csharp \
 -o /local/out --additional-properties=packageName=Coflnet.Sky.Bazaar.Flipper.Client,packageVersion=$VERSION,licenseId=MIT
 
 cd out
-sed -i 's/GIT_USER_ID/Coflnet/g' src/Coflnet.Sky.Bazaar.Flipper.Client/Coflnet.Sky.Bazaar.Flipper.Client.csproj
-sed -i 's/GIT_REPO_ID/SkyBazaarFlipper/g' src/Coflnet.Sky.Bazaar.Flipper.Client/Coflnet.Sky.Bazaar.Flipper.Client.csproj
-sed -i 's/>OpenAPI/>Coflnet/g' src/Coflnet.Sky.Bazaar.Flipper.Client/Coflnet.Sky.Bazaar.Flipper.Client.csproj
+path=src/Coflnet.Sky.Bazaar.Flipper.Client/Coflnet.Sky.Bazaar.Flipper.Client.csproj
+sed -i 's/GIT_USER_ID/Coflnet/g' $path
+sed -i 's/GIT_REPO_ID/SkyBazaarFlipper/g' $path
+sed -i 's/>OpenAPI/>Coflnet/g' $path
+sed -i 's@annotations</Nullable>@annotations</Nullable>\n    <PackageReadmeFile>README.md</PackageReadmeFile>@g' $path
+sed -i 's@Remove="System.Web" />@Remove="System.Web" />\n    <None Include="../../../../README.md" Pack="true" PackagePath="\"/>@g' $path
 
 dotnet pack
-cp src/Coflnet.Sky.Bazaar.Flipper.Client/bin/Debug/Coflnet.Sky.Bazaar.Flipper.Client.*.nupkg ..
+cp src/Coflnet.Sky.Bazaar.Flipper.Client/bin/Release/Coflnet.Sky.Bazaar.Flipper.Client.*.nupkg ..
