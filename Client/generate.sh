75e75a26bb199e78c6522313b15d67e857fabe05
Äkwav <ekwav@coflnet.com>
2025-06-19 18:10:17 +0200

    update packages

--- Client/generate.sh
+++ Client/generate.sh
@@ -1,17 +1,19 @@
-VERSION=0.1.0
+VERSION=0.1.2
+PACKAGE_NAME=Coflnet.Sky.Bazaar.Flipper.Client
 
 docker run --rm -v "${PWD}:/local" --network host -u $(id -u ${USER}):$(id -g ${USER})  openapitools/openapi-generator-cli generate \
 -i http://localhost:5033/swagger/v1/swagger.json \
 -g csharp \
--o /local/out --additional-properties=packageName=Coflnet.Sky.Bazaar.Flipper.Client,packageVersion=$VERSION,licenseId=MIT
+-o /local/out --additional-properties=packageName=$PACKAGE_NAME,packageVersion=$VERSION,licenseId=MIT,targetFramework=net8.0,library=restsharp
 
 cd out
-path=src/Coflnet.Sky.Bazaar.Flipper.Client/Coflnet.Sky.Bazaar.Flipper.Client.csproj
+path=src/$PACKAGE_NAME/$PACKAGE_NAME.csproj
 sed -i 's/GIT_USER_ID/Coflnet/g' $path
 sed -i 's/GIT_REPO_ID/SkyBazaarFlipper/g' $path
 sed -i 's/>OpenAPI/>Coflnet/g' $path
 sed -i 's@annotations</Nullable>@annotations</Nullable>\n    <PackageReadmeFile>README.md</PackageReadmeFile>@g' $path
-sed -i 's@Remove="System.Web" />@Remove="System.Web" />\n    <None Include="../../../../README.md" Pack="true" PackagePath="\"/>@g' $path
+sed -i '34i    <None Include="../../../../README.md" Pack="true" PackagePath="\"/>' $path
 
 dotnet pack
-cp src/Coflnet.Sky.Bazaar.Flipper.Client/bin/Release/Coflnet.Sky.Bazaar.Flipper.Client.*.nupkg ..
+cp src/$PACKAGE_NAME/bin/Release/$PACKAGE_NAME.*.nupkg ..
+dotnet nuget push ../$PACKAGE_NAME.$VERSION.nupkg --api-key $NUGET_API_KEY --source "nuget.org" --skip-duplicate
